5374cf09a306fac4369d54aee227b8b5addc6a54
yongrenwang <czie.alexmak@gmail.com>
2017-02-17 20:32:55 +0800

    Refactored the code so that a size property is added dynamically to the aniamls by way of a pre function; also, stored animal data in an array, and used the create function called on the Animal schema to save the Animal instances

--- mongoose_sandbox.js
+++ mongoose_sandbox.js
@@ -18,17 +18,27 @@ db.once('open',function(){
 
 	var AnimalSchema = new Schema({
 		type: {type: String, 	default: "goldfish"},
-		size: {type: String, 	default: "small"},
+		size: String,
 		color: {type: String, 	default: "golden" },
 		mass: {type: Number, 	default: 0.007 },
 		name: {type: String, 	default: "Angela"}
+	});
+
+	AnimalSchema.pre("save",function(next){
+		if(this.mass >= 100){
+			this.size = "big";
+		} else if (this.mass >= 5 && this.mass < 100){
+			this.size = "medium";
+		} else {
+			this.size = "small";
+		}
+		next();
 	});
 
 	var Animal = mongoose.model("Animal", AnimalSchema);
 
 	var elephant = new Animal({
 		type: "elephant",
-		size: "big",
 		color: "gray",
 		mass: 6000,
 		name: "Lawrence"
@@ -39,7 +49,6 @@ db.once('open',function(){
 
 	var whale = new Animal({
 		type: "whale",
-		size: "big",
 		mass: 190500,
 		name: "Fig"
 
@@ -50,19 +59,40 @@ db.once('open',function(){
 	// 	resolve();
 	// };
 
+	var animalData = [
+		{
+			type: "mouse",
+			color: "gray",
+			mass: 0.035,
+			name: "Marvin"
+		},
+
+		{
+			type: "nutria",
+			color: "brown",
+			mass: 6.35,
+			name: "Gretchen"
+		},
+
+		{
+			type: "wolf",
+			color: "gray",
+			mass: 45,
+			name: "Iris"
+		},
+		elephant,
+		animal,
+		whale
+	];
+
 
 	Animal.remove({},function(err){
 		if(err) console.error(err);
-		elephant.save(function(err){
-		if(err) console.error("Save Failed.",err);
-			animal.save(function(err){
-				if(err) console.error("Save Failed",err);
-				whale.save(function(err){
-					if(err)console.err("Saved Failed", err);
-
-					Animal.find({size:"big"},function(err, animals){
+		Animal.create(animalData, function(err,animals){
+				if(err)console.err("Saved Failed", err);
+				Animal.find({},function(err, animals){
 						animals.forEach(function(animal){
-							console.log(animal.name + " the " + animal.color + " " + animal.type);
+							console.log(animal.name + " the " + animal.color + " " + animal.type + " is a " + animal.size + "-sized animal");
 						});
 						db.close(function(){
 							console.log("db connection closed");
@@ -71,12 +101,11 @@ db.once('open',function(){
 					});
 				});
 					
-				});
+	});
 				
-			});
-	});
 
-	});
+
+});
 
 	
 
