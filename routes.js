20217cd8cd8269f438b8423d23ab635c01612e40
yongrenwang <czie.alexmak@gmail.com>
2017-02-17 14:30:04 +0800

    Added routes for creating, editing, and deleting answers to questions, as well as for voting up or down on a question

--- routes.js
+++ routes.js
@@ -24,14 +24,59 @@ router.post("/",function(req,res){
 
 // GET /questions/:id
 // Route for a specific question 
-router.get("/:id",function(req,res){
+router.get("/:qID",function(req,res){
 	res.json({
-		response: "You sent me a GET request for " + req.params.id});
+		response: "You sent me a GET request for " + req.params.qID});
 });
 
 
+// POST /questions/:id/answers
+// Route for creating an answer
+router.post("/:qID/answers",function(req,res){
+	res.json({
+		response: "You sent me a POST request to /answers",
+		questionId: req.params.qID,
+		body: req.body 
+	});
+});
 
 
+// PUT /questions/:id/answers
+// Route for editing an anwer
+router.put("/:qID/answers/:aID",function(req,res){
+	res.json({
+		response: "You sent me a PUT request to /answers",
+		questionId: req.params.qID,
+		answerId: req.params.aID,
+		body: req.body 
+	});
+});
+
+
+// DELETE /questions/:id/answers
+// Route for deleting an anwer
+router.delete("/:qID/answers/:aID",function(req,res){
+	res.json({
+		response: "You sent me a DELETE request to /answers",
+		questionId: req.params.qID,
+		answerId: req.params.aID,
+		body: req.body 
+	});
+});
+
+
+// POST /questions/:id/answers/vote-up
+// POST /questions/:id/answers/vote-down
+// Route for voting on an answer
+router.post("/:qID/answers/:aID/vote-:dir",function(req,res){
+	res.json({
+		response: "You sent me a POST request to /vote-" + req.params.dir,
+		questionId: req.params.qID,
+		answerId: req.params.aID,
+		body: req.body,
+		vote: req.params.dir
+	});
+});
 
 
 
