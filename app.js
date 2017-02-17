363499ffa6cc71560a2afbf0fadaf4a4421b38c1
yongrenwang <czie.alexmak@gmail.com>
2017-02-17 13:27:19 +0800

    Added two custom middleware test functions that log JSON data in the request body; one method is called before the third-party body-parser middleware is called on the app, and the other after introducing the third-party middleware; without the body-parser middleware, application data sent in the body of the REQUEST object in JSON format cannot be parsed, and the request object is found to have no body object

--- app.js
+++ app.js
@@ -2,13 +2,22 @@
 
 var express = require('express');
 var app = express();
+var jsonParser = require('body-parser').json;
 
-app.use(function(req,res,next){
-	//do something
+var jsonCheck = function(req,res,next){
+	if(req.body){
+		console.log("The sky is", req.body.color);
+	} else {
+		console.log("There is no body property on the request");
+	}
 	next();
-});
+};
 
+app.use(jsonCheck);
 
+app.use(jsonParser());
+
+app.use(jsonCheck);
 
 var port = process.env.PORT || 3000;
 
