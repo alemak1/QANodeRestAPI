2b32523c202aa8d0ee28d84a22543f99d37f5c44
yongrenwang <czie.alexmak@gmail.com>
2017-02-17 14:12:51 +0800

    Created routes for post and get requests for all questions as well as GET requests for a specific question

--- routes.js
+++ routes.js
@@ -4,9 +4,30 @@ var express = require('express');
 var router = express.Router();
 
 
+// GET /questions
+// Route for the questions collection
+router.get("/",function(req,res){
+	res.json({response: "You sent me a GET request"});
+});
+
+
+// POST /questions
+// Route for creating a question
+router.post("/",function(req,res){
+	res.json({
+		response: "You sent me a POST request",
+		body: req.body 
+	});
+});
 
 
 
+// GET /questions/:id
+// Route for a specific question 
+router.get("/:id",function(req,res){
+	res.json({
+		response: "You sent me a GET request for " + req.params.id});
+});
 
 
 
