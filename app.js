ccc9c5043aeaf55e40103c9e237e762c43b99224
yongrenwang <czie.alexmak@gmail.com>
2017-02-17 13:10:14 +0800

    Defined a middleware function that logs the value of a query parameter to the console; query parameter is accessible via the name of the property in the query object

--- app.js
+++ app.js
@@ -4,15 +4,11 @@ var express = require('express');
 var app = express();
 
 app.use(function(req,res,next){
-	req.myMessage = "Hello Middleware 2";
-	console.log("First piece of middleware");
+	console.log("The color is ", req.query.color);
 	next();
 });
 
-app.use(function(req,res,next){
-	console.log(req.myMessage);
-	next();
-});
+
 
 var port = process.env.PORT || 3000;
 
