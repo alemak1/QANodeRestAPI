8d108dcebf90a0ea13758ee6eac8aa5c59b70234
yongrenwang <czie.alexmak@gmail.com>
2017-02-17 14:54:22 +0800

    Deleted nonexistent object

--- routes.js
+++ routes.js
@@ -7,7 +7,6 @@ var router = express.Router();
 // GET /questions
 // Route for the questions collection
 router.get("/",function(req,res){
-	var o = obj.prop;
 	res.json({response: "You sent me a GET request"});
 });
 
