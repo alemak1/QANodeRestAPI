27e671293b9a02622a7d02224c665c502b1e6ad3
yongrenwang <czie.alexmak@gmail.com>
2017-02-17 14:53:48 +0800

    Created a nonexist object in callback function for get request to /questions

--- routes.js
+++ routes.js
@@ -7,6 +7,7 @@ var router = express.Router();
 // GET /questions
 // Route for the questions collection
 router.get("/",function(req,res){
+	var o = obj.prop;
 	res.json({response: "You sent me a GET request"});
 });
 
