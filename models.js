8b25e3ab27b4d60a6c22735746ab05c433fb1ab1
yongrenwang <czie.alexmak@gmail.com>
2017-02-18 11:46:51 +0800

    Implemented a sorting function to be used as argument for calling sort on the answers array for question instance; define a pre-save stub function that will sort questions

--- models.js
+++ models.js
@@ -2,6 +2,24 @@
 
 var mongoose = require('mongoose');
 
+var answers = function(a,b){
+	// - a before b
+	// 0 no change
+	//+ positive a after b
+
+	if(a.votes === b.votes){
+		if(a.updatedAt > b.updatedAt){
+			return -1;
+		} else if (a.updatedAt < b.updatedAt){
+			return 1;
+		} else {
+			return 0;
+		}
+	}
+
+	return b.votes - a.votes;
+}
+
 var Schema = mongoose.Schema;
 
 var AnswerSchema = new Schema({
@@ -16,6 +34,11 @@ var QuestionSchema = new Schema({
 	text: String, 
 	createdAt: {type: Date, default: Date.now},
 	answers: [AnswerSchema]
+});
+
+QuestionSchema.pre("save",function(next){
+	this.answers.sort();
+	next();
 });
 
 var Question = mongoose.model("Question",QuestionSchema);
