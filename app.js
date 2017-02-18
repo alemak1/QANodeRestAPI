5f7d33338087725d77c3f69a0cf77682df952263
yongrenwang <czie.alexmak@gmail.com>
2017-02-18 11:22:01 +0800

    Added database connection code to main app

--- app.js
+++ app.js
@@ -11,6 +11,21 @@ app.use(logger('dev'));
 
 app.use(jsonParser());
 
+var mongoose = require('mongoose');
+
+mongoose.connect("mongodb://localhost:27017/qa");
+
+var db = mongoose.connection;
+
+db.on('error',function(err){
+	console.error("connection error: ", err);
+});
+
+db.once('open',function(){
+	console.log("db connection successful");
+	//All database communication goes here
+}
+
 app.use("/questions",routes);
 
 //catch 404 and return to error handler
